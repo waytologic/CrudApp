3c7ad91f16f08bb0556d629fac680de36a663904
Way2logic <128026796+waytologic@users.noreply.github.com>
2023-09-06 21:51:36 +0530

    Update common.service.ts

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -5,20 +5,25 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CommonService {
-
+  const baseURL = "https://my-json-server.typicode.com/waytologic/arpino";
+  
   constructor(private _http:HttpClient) { }
 
   Createuser(user:any) {
-    return this._http.post('http://localhost:3000/users',user);
+    //return this._http.post('http://localhost:3000/users',user);
+    return this._http.post(this.baseURL+'/users',user);
   }
   Getalluser() {
-    return this._http.get('http://localhost:3000/users')
+    //return this._http.get('http://localhost:3000/users')
+    return this._http.get(this.baseURL+'/users')
   }
   Updateuser(user:any) {
-    return this._http.put("http://localhost:3000/users/" +user.id,user)
+    //return this._http.put("http://localhost:3000/users/" +user.id,user)
+    return this._http.put(this.baseURL+"/users/" +user.id,user)  
   }
   Deleteuser(user:any) {
-    return this._http.delete("http://localhost:3000/users/" +user.id)
+    //return this._http.delete("http://localhost:3000/users/" +user.id)
+    return this._http.delete(this.baseURL+"/users/" +user.id)
   }
 
 }
