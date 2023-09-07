dd3df182f06c4234f3850930d1fa65bfad5bdaea
Way2logic <128026796+waytologic@users.noreply.github.com>
2023-09-07 07:42:07 +0530

    Update common.service.ts

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const baseUrl = 'https://my-json-server.typicode.com/waytologic/arpino';
+
 @Injectable({
   providedIn: 'root'
 })
-export class CommonService {
-  const baseURL = "https://my-json-server.typicode.com/waytologic/arpino";
+export class CommonService { 
   
   constructor(private _http:HttpClient) { }
 
   Createuser(user:any) {
     //return this._http.post('http://localhost:3000/users',user);
-    return this._http.post(this.baseURL+'/users',user);
+    return this._http.post(baseUrl+'/users',user);
   }
   Getalluser() {
     //return this._http.get('http://localhost:3000/users')
-    return this._http.get(this.baseURL+'/users')
+    return this._http.get(baseUrl+'/users')
   }
   Updateuser(user:any) {
     //return this._http.put("http://localhost:3000/users/" +user.id,user)
-    return this._http.put(this.baseURL+"/users/" +user.id,user)  
+    return this._http.put(baseUrl+"/users/" +user.id,user)  
   }
   Deleteuser(user:any) {
     //return this._http.delete("http://localhost:3000/users/" +user.id)
-    return this._http.delete(this.baseURL+"/users/" +user.id)
+    return this._http.delete(baseUrl+"/users/" +user.id)
   }
 
 }
